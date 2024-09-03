38d8556a8cd643040d812d50c27958acb5ae7fd2
Robson Alencastro <90284550+Alencast@users.noreply.github.com>
2024-09-03 15:44:32 -0300

    Update script.js

--- main/script.js
+++ main/script.js
@@ -1,20 +1,5 @@
-const femininos = document.querySelector('#femininos');
-const masculinos = document.querySelector('#masculinos');
+const femininos = document.querySelector('#femininos')
 
-femininos.addEventListener('click', (event) => {
-    event.preventDefault(); // Evita bug de comportamento padrão do link
-    const section = document.querySelector('#femininos');
-    window.scrollTo({
-        top: section.offsetTop,
-        behavior: 'smooth' // deixa a rolagem pra baixo mais suave, tava muito brusca
-    });
-});
-
-masculinos.addEventListener('click', (event) => {
-    event.preventDefault(); 
-    const section = document.querySelector('#masculinos');
-    window.scrollTo({
-        top: section.offsetTop,
-        behavior: 'smooth' 
-    });
-});
+femininos.addEventListener('click', () =>{
+    window.scroll({top: 1.8*window.innerHeight})
+})
